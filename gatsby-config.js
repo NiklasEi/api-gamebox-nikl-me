6580eb4931cb4b7441d9b6f645970500ff6031a0
Niklas Eicker <git@nikl.me>
2020-03-26 22:26:41 +0100

    create gatsby site serving the previous static api

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  plugins: [
+    // {
+    //   resolve: 'gatsby-source-filesystem',
+    //   options: {
+    //     name: 'content',
+    //     path: `${__dirname}/src/content`
+    //   }
+    // },
+    'gatsby-plugin-typescript'
+  ]
+};
